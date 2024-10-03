42216827b8aee65dbbdb4e3a91a1ef90dca72f38
mikael-v <mikael-v@outlook.com>
2024-10-03 15:24:38 +0100

    initial battle in terminal complete with inquirer

--- battle.js
+++ battle.js
@@ -1,9 +1,16 @@
+import { rawlist, input } from "@inquirer/prompts";
+
+function delay(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
 class Pokemon {
-  constructor(name, hitPoints, attackDMG, move) {
+  constructor(name, hitPoints, attackDMG, move, trainerName = "") {
     this.name = name;
     this.hitPoints = hitPoints;
     this.attackDMG = attackDMG;
     this.move = move;
+    this.trainerName = trainerName;
   }
 
   takeDamage(num) {
@@ -21,89 +28,84 @@ class Pokemon {
     }
     return false;
   }
+
+  displayName() {
+    return this.trainerName ? `${this.trainerName}'s ${this.name}` : this.name;
+  }
 }
 
 class Fire extends Pokemon {
-  constructor(name, hitPoints, attackDMG, move) {
-    super(name, hitPoints, attackDMG, move);
+  constructor(name, hitPoints, attackDMG, move, trainerName) {
+    super(name, hitPoints, attackDMG, move, trainerName);
     this.type = "Fire";
   }
-  isEffectiveAgainst(current, given) {
-    if (current.type === "Fire" && given.type === "Grass") {
-      return true;
-    }
-    return false;
+
+  isEffectiveAgainst(opposing) {
+    return this.type === "Fire" && opposing.type === "Grass";
   }
-  isWeakTo(current, given) {
-    if (current.type === "Fire" && given.type === "Water") {
-      return true;
-    }
-    return false;
+
+  isWeakTo(opposing) {
+    return this.type === "Fire" && opposing.type === "Water";
   }
 }
+
 class Grass extends Pokemon {
-  constructor(name, hitPoints, attackDMG, move) {
-    super(name, hitPoints, attackDMG, move);
+  constructor(name, hitPoints, attackDMG, move, trainerName) {
+    super(name, hitPoints, attackDMG, move, trainerName);
     this.type = "Grass";
   }
-  isEffectiveAgainst(current, given) {
-    if (current.type === "Grass" && given.type === "Water") {
-      return true;
-    }
-    return false;
+
+  isEffectiveAgainst(opposing) {
+    return this.type === "Grass" && opposing.type === "Water";
   }
-  isWeakTo(current, given) {
-    if (current.type === "Grass" && given.type === "Fire") {
-      return true;
-    }
-    return false;
+
+  isWeakTo(opposing) {
+    return this.type === "Grass" && opposing.type === "Fire";
   }
 }
+
 class Water extends Pokemon {
-  constructor(name, hitPoints, attackDMG, move) {
-    super(name, hitPoints, attackDMG, move);
+  constructor(name, hitPoints, attackDMG, move, trainerName) {
+    super(name, hitPoints, attackDMG, move, trainerName);
     this.type = "Water";
   }
-  isEffectiveAgainst(current, given) {
-    if (current.type === "Water" && given.type === "Fire") {
-      return true;
-    }
-    return false;
+
+  isEffectiveAgainst(opposing) {
+    return this.type === "Water" && opposing.type === "Fire";
   }
-  isWeakTo(current, given) {
-    if (current.type === "Water" && given.type === "Grass") {
-      return true;
-    }
-    return false;
+
+  isWeakTo(opposing) {
+    return this.type === "Water" && opposing.type === "Grass";
   }
 }
+
 class Normal extends Pokemon {
-  constructor(name, hitPoints, attackDMG, move) {
-    super(name, hitPoints, attackDMG, move);
-    this.type = "Water";
+  constructor(name, hitPoints, attackDMG, move, trainerName) {
+    super(name, hitPoints, attackDMG, move, trainerName);
+    this.type = "Normal";
   }
 }
+
 class Charmander extends Fire {
-  constructor(name, hitPoints, attackDMG, move, type) {
-    super(name, hitPoints, attackDMG, move, type);
-    this.move = "ember";
+  constructor(trainerName) {
+    super("Charmander", 100, 20, "ember", trainerName);
   }
 }
 class Squirtle extends Water {
-  constructor(name, hitPoints, attackDMG, move, type) {
-    super(name, hitPoints, attackDMG, move, type);
+  constructor(name, hitPoints, attackDMG, move, type, trainerName) {
+    super(name, hitPoints, attackDMG, move, type, trainerName);
     this.move = "water gun";
   }
 }
 class Bulbasaur extends Grass {
-  constructor(name, hitPoints, attackDMG, move, type) {
-    super(name, hitPoints, attackDMG, move, type);
+  constructor(name, hitPoints, attackDMG, move, type, trainerName) {
+    super(name, hitPoints, attackDMG, move, type, trainerName);
     this.move = "vine rip";
   }
 }
 class Rattata extends Normal {
-  constructor(name, hitPoints, attackDMG, move, type) {
-    super(name, hitPoints, attackDMG, move, type);
+  constructor(name, hitPoints, attackDMG, move, type, trainerName) {
+    super(name, hitPoints, attackDMG, move, type, trainerName);
   }
 }
 
@@ -111,50 +113,101 @@ function damageDealt(current, opposing) {
   let num;
   if (current.isEffectiveAgainst(opposing)) {
     num = current.attackDMG * 1.25;
-    console.log(`${current.name}'s attack is super effective!`);
+    console.log(`${current.displayName()}'s attack is super effective!`);
   } else if (current.isWeakTo(opposing)) {
     num = current.attackDMG * 0.75;
-    console.log(`${current.name}'s attack is not very effective...`);
+    console.log(`${current.displayName()}'s attack is not very effective...`);
   } else {
     num = current.attackDMG;
   }
   return num;
 }
 
-function battleTurn(attacker, defender) {
-  console.log(`${attacker.name} attacks ${defender.name}!`);
-  let damage = damageDealt(attacker, defender);
+const playerName = await input({ message: "Enter you name:" });
 
+async function battleTurn(attacker, defender) {
+  console.log(`${attacker.displayName()} attacks ${defender.displayName()}!`);
+  await delay(1000);
+
+  let damage = damageDealt(attacker, defender);
   defender.takeDamage(damage);
+
   console.log(
-    `${defender.name} takes ${damage} damage! Remaining HP: ${defender.hitPoints}`
+    `${defender.displayName()} takes ${damage} damage! Remaining HP: ${
+      defender.hitPoints
+    }`
   );
+  console.log(" ");
+  await delay(1000);
 
   if (defender.hasFainted()) {
-    console.log(`${defender.name} has fainted! ${attacker.name} wins!`);
+    console.log(
+      `${defender.displayName()} has fainted! ${attacker.displayName()} wins!`
+    );
     return true;
   }
+
   return false;
 }
 
-function battle(pokemon1, pokemon2) {
+async function battle(pokemon1, pokemon2) {
   let battleOver = false;
   let turn = 1;
 
   while (!battleOver) {
     console.log(`Turn ${turn}:`);
+    await delay(1000);
 
-    battleOver = battleTurn(pokemon1, pokemon2);
+    battleOver = await battleTurn(pokemon1, pokemon2);
     if (battleOver) break;
 
-    battleOver = battleTurn(pokemon2, pokemon1);
+    battleOver = await battleTurn(pokemon2, pokemon1);
     if (battleOver) break;
 
     turn++;
   }
 }
 
-module.exports = {
+function createPokemon(choice, trainerName = "") {
+  switch (choice) {
+    case "Charmander":
+      return new Charmander(trainerName);
+    case "Squirtle":
+      return new Squirtle(trainerName);
+    case "Bulbasaur":
+      return new Bulbasaur(trainerName);
+    case "Rattata":
+      return new Rattata(trainerName);
+    default:
+      throw new Error("Unknown Pokémon");
+  }
+}
+
+const choice = await rawlist({
+  message: "Choose your fighter!",
+  choices: [
+    { name: `${playerName}'s Charmander`, value: "Charmander" },
+    { name: `${playerName}'s Squirtle`, value: "Squirtle" },
+    { name: `${playerName}'s Bulbasaur`, value: "Bulbasaur" },
+    { name: `${playerName}'s Rattata`, value: "Rattata" },
+  ],
+});
+
+const opponent = await rawlist({
+  message: "Choose your Opponent!",
+  choices: [
+    { name: "Charmander", value: "Charmander" },
+    { name: "Squirtle", value: "Squirtle" },
+    { name: "Bulbasaur", value: "Bulbasaur" },
+    { name: "Rattata", value: "Rattata" },
+  ],
+});
+
+const playerPokemon = createPokemon(choice, playerName);
+const opponentPokemon = createPokemon(opponent);
+battle(playerPokemon, opponentPokemon);
+
+export {
   Pokemon,
   Fire,
   Grass,
